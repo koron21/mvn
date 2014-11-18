cf22809b459a5def2525ff309f96da70cf39eddb
koron21 <koron21@iCloud.com>
2014-11-19 00:59:36 +0900

    愛ちゃんモデル差し替え、目パーツ表情対応途中

--- game/Assets/Script/PatternAnimation.cs
+++ game/Assets/Script/PatternAnimation.cs
@@ -18,6 +18,10 @@ public class PatternAnimation : MonoBehaviour
     private Transform leg;
     private Transform[] legParts;
 
+    private Transform eyes;
+    private Transform[] eyesNormal;
+    private Transform[] eyesReaction;
+
     // Use this for initialization
     void Start()
     {
@@ -26,7 +30,7 @@ public class PatternAnimation : MonoBehaviour
         if (legAnimationLength <= 0.0f)
             legAnimationLength = 1.0f;
 
-        // find leg, and animation parts
+        // find legs, and animation parts
         leg = transform.FindChild("legs");
         if (leg)
         {
@@ -34,6 +38,33 @@ public class PatternAnimation : MonoBehaviour
             for (int i = 0; i < leg.childCount; i ++)
             {
                 legParts[i] = leg.GetChild(i);
+            }
+        }
+
+        // find eyes
+        eyes = transform.FindChild("eyes");
+        if (eyes)
+        {
+            // normal
+            Transform t = eyes.FindChild("normal");
+            if (t)
+            {
+                eyesNormal = new Transform[t.childCount];
+                for (int i = 0; i < t.childCount; i++ )
+                {
+                    eyesNormal[i] = t.GetChild(i);
+                }
+            }
+
+            // reaction
+            t = eyes.FindChild("reaction");
+            if (t)
+            {
+                eyesReaction = new Transform[t.childCount];
+                for (int i = 0; i < t.childCount; i ++)
+                {
+                    eyesReaction[i] = t.GetChild(i);
+                }
             }
         }
    }
